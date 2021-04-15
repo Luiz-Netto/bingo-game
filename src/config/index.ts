5f76119ac5350641fc7525b9260c25b3ec903074
Luiz.venancio <luiznetto@protonmail.com>
2021-04-14 23:07:06 -0300

    feature: navigation working on routes

---
+++ src/config/index.ts
@@ -0,0 +1,3 @@
+import {AppNavigation} from './routes';
+
+export {AppNavigation};
--- src/config/routes.tsx
+++ src/config/routes.tsx
@@ -1,6 +1,27 @@
 import React from 'react';
-import {StyleSheet, View} from 'react-native';
+import {StyleSheet, Text, View} from 'react-native';
+import {createStackNavigator} from '@react-navigation/stack';
 
-const MainRoute = () => {
-  return <View />;
+const HomeComponent = () => {
+  return (
+    <View style={styles.homeScreen}>
+      <Text> HomeScreen</Text>
+    </View>
+  );
 };
+
+const Stack = createStackNavigator();
+
+const AppNavigation = () => {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="Home" component={HomeComponent} />
+    </Stack.Navigator>
+  );
+};
+
+const styles = StyleSheet.create({
+  homeScreen: {flex: 1, alignItems: 'center', justifyContent: 'center'},
+});
+
+export {AppNavigation};
